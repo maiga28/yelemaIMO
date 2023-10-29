dd5d62601833d1c615f356f8ca6e5211ff39495f
maiga28 <sidimaiga.work@gmail.com>
2023-10-30 00:54:07 +0100

    Résolution des conflits

---
+++ Desktop/yelemaIMO/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+        './templates/**/**/*.html',
+        './node_modules/flowbite/**/*.js',
+        'node_modules/preline/dist/*.js',
+
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('flowbite/plugin'),
+        require('preline/plugin'),
+    ],
+}
